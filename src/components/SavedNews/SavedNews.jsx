6ead04d681b66e53d79ca50da41691f6d671d6ef
Dmitriy <dmitriy.slinkovenko@gmail.com>
2024-06-24 10:28:28 -0400

    Final project, fixed /saved-page positioning and fonts, working on BEM

--- src/components/SavedNews/SavedNews.jsx
+++ src/components/SavedNews/SavedNews.jsx
@@ -2,7 +2,7 @@ import "./SavedNews.css";
 
 export default function SavedNews() {
   return (
-    <div className="saved-news__section">
+    <div className="saved-news">
       <div className="saved-news__containter"></div>
     </div>
   );
